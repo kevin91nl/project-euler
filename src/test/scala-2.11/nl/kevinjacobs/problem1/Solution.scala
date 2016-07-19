6fa6d3cd41779a325fc2d0ada19802d28dfea220
Kevin Jacobs <kevin91nl@gmail.com>
2016-07-19 07:58:33 +0200

    Initial commit

---
+++ src/test/scala-2.11/nl/kevinjacobs/problem1/Solution.scala
@@ -0,0 +1,14 @@
+package nl.kevinjacobs.problem1
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by kevin on 19-7-16.
+  */
+class Solution extends FlatSpec with Matchers {
+
+    "1+1" should "equal 2" in {
+        assert(1+1 == 2)
+    }
+
+}
